e5b51305030b6e7a0f2bcfd8fd15f8ad78c921f2
AnnaSvirloch <veve.bassoon@gmail.com>
2026-05-25 14:57:12 +0200

    up

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-
-});
-
-
 const burger = document.querySelector(".burger");
 const navMenu = document.querySelector("nav ul");
-
-burger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
 
 burger.addEventListener("click", () => {
 
